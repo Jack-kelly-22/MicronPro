4d1fb92bc570dd1323615accc16198bdad311ed4
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-07-01 05:21:37 -0700

    test deploy

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NO_RELOAD=""
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "use: ./run.sh {app.py} [-r]"
+    exit 1
+fi
+
+if [[ $# -eq 2 && $2 == "-r" ]]; then
+    NO_RELOAD="--no-reload"
+fi
+
+echo "Application is going live"
+export FLASK_APP=$1
+export FLASK_ENV=development
+flask run $NO_RELOAD
+
