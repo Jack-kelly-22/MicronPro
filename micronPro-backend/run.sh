a36dc76fa17b96490b52c191ee8c4f585217c7ce
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-07-01 04:38:07 -0700

    run fix

--- micronPro-backend/run.sh
+++ micronPro-backend/run.sh
@@ -3,7 +3,7 @@
 NO_RELOAD=""
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
-    echo "use: ./run.sh {worker_app.py} [-r]"
+    echo "use: ./run.sh {app.py} [-r]"
     exit 1
 fi
 
@@ -15,3 +15,4 @@ echo "Application is going live"
 export FLASK_APP=$1
 export FLASK_ENV=development
 flask run $NO_RELOAD
+
