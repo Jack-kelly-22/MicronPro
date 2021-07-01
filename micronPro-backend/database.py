08678ddb555c86259cd37e6a89acaa34da35f191
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-07-01 04:35:32 -0700

    remove url

--- micronPro-backend/database.py
+++ micronPro-backend/database.py
@@ -9,21 +9,21 @@ logger = logging.getLogger("root")
 
 class MicroDatabase:
     def __init__(self,config):
-        database, user, password = (
+        user, password = (
          
             config["MongoDB"]["user"],
             config["MongoDB"]["password"],
         )
         if (
-            database == "YOURURLHERE"
-            or user == "YOURUSERHERE"
+            user == "YOURUSERHERE"
             or password == "YOURPASSWORDHERE"
         ):
             logger.critical("user or password has not been changed!")
         else:
-            logger.info("database has been successfully hooked up")
+            logger.info("database credentials loaded")
         self.client = MongoClient(
             "mongodb+srv://" + user +":" + password + "@maincluster.btvwv.mongodb.net"
         )
+        logger.info("database responded")
     
-    
+    def update_job(job):
