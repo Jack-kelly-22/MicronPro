023bfd9d4cceed24acc601af2f20efa79659e1e2
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-06-30 23:28:15 -0700

    created backend_vars file

--- micronPro-backend/backend_vars.py
+++ micronPro-backend/backend_vars.py
@@ -1,3 +1,7 @@
+from config import BackendConfiguration
+import utils.logger as logger
 log = logger.setup_logger("root")
-configFile = threadConfiguration()
+configFile = BackendConfiguration()
 log.debug("initalized logger")
+
+workers = {5000: "0.0.0.0"}
