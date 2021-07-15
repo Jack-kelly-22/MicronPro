104253da3fb897c8f598e90883226d66ce0ad239
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-07-15 09:54:51 -0700

    added jwt config

--- micronPro-backend/app.py
+++ micronPro-backend/app.py
@@ -4,12 +4,14 @@ from flask_jwt_extended import JWTManager
 from backend_vars import configFile, log
 from endpoints.job_blueprint import job_blueprint
 from endpoints.worker_blueprint import worker_blueprint
+from endpoints.user_blueprint import user_blueprint
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config["JWT_SECRET_KEY"] = configFile.get_configuration()["JWT"]["secret"]
+    jwt = JWTManager(app)
     app.register_blueprint(job_blueprint)
     app.register_blueprint(worker_blueprint)
-    jwt = JWTManager(app)
+    app.register_blueprint(user_blueprint)
     return app
